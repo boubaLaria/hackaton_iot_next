1a869dad6340a487e0eac8e07b00b2153cc4cfd4
LordTibu <nicolaspatino77@gmail.com>
2025-03-17 14:42:14 +0100

    clean index page

--- src/components/Layouts/header/user-info/index.tsx
+++ src/components/Layouts/header/user-info/index.tsx
@@ -7,7 +7,6 @@ import {
   DropdownTrigger,
 } from "@/components/ui/dropdown";
 import { cn } from "@/lib/utils";
-import Image from "next/image";
 import Link from "next/link";
 import { useState } from "react";
 import { LogOutIcon, SettingsIcon, UserIcon } from "./icons";
@@ -18,7 +17,6 @@ export function UserInfo() {
   const USER = {
     name: "John Smith",
     email: "johnson@nextadmin.com",
-    img: "/images/user/user-03.png",
   };
 
   return (
@@ -27,14 +25,6 @@ export function UserInfo() {
         <span className="sr-only">My Account</span>
 
         <figure className="flex items-center gap-3">
-          <Image
-            src={USER.img}
-            className="size-12"
-            alt={`Avatar of ${USER.name}`}
-            role="presentation"
-            width={200}
-            height={200}
-          />
           <figcaption className="flex items-center gap-1 font-medium text-dark dark:text-dark-6 max-[1024px]:sr-only">
             <span>{USER.name}</span>
 
@@ -57,15 +47,6 @@ export function UserInfo() {
         <h2 className="sr-only">User information</h2>
 
         <figure className="flex items-center gap-2.5 px-5 py-3.5">
-          <Image
-            src={USER.img}
-            className="size-12"
-            alt={`Avatar for ${USER.name}`}
-            role="presentation"
-            width={200}
-            height={200}
-          />
-
           <figcaption className="space-y-1 text-base font-medium">
             <div className="mb-2 leading-none text-dark dark:text-white">
               {USER.name}
